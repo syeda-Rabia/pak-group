07bd56904ed9d45db3513f888c5f3a66fe1f565d
syeda-Rabia <rabiagillani899@gmail.com>
2020-12-22 18:04:43 +0500

    modal styles updated

--- src/assests/constants/modal.js
+++ src/assests/constants/modal.js
@@ -2,28 +2,37 @@ export const ModalData = [
   {
     id: "1",
     Name: "Rabia",
+    Last_Name: "waheed",
     Email: "Rabia@gmail.com",
+    Gender: [ "Female", "Male"],
+    Contact: ["+92309874556"],
     Type: ["Employee", "Admin"],
   },
   {
     id: "2",
     Name: "Sana",
-
+    Last_Name: "syed",
     Email: "Sana@gmail.com",
+    Gender: ["Female", "Male"],
+    Contact: ["+92309874556"],
     Type: ["Employee", "Admin"],
   },
   {
     id: "3",
     Name: "Atif",
-
-    Email: "Atif@gmail.com",
+    Last_Name: "riaz",
+    Email: "atif@gmail.com",
+    Gender: ["Male", "Female"],
+    Contact: ["+92309874556"],
     Type: ["Employee", "Admin"],
   },
   {
     id: "4",
     Name: "Atif",
-
-    Email: "Atif@gmail.com",
+    Last_Name: "riaz",
+    Email: "atif@gmail.com",
+    Gender: ["Male", "Female"],
+    Contact: ["+92309874556"],
     Type: ["Employee", "Admin"],
   },
 ];
