fa03e25be7e495d0ad6a3173daa77ecb68032b61
Qasim-Yousaf <qasim.muhammadyousaf@gmail.com>
2021-01-11 18:14:47 +0500

    token issue resolved

--- src/utils/Config.js
+++ src/utils/Config.js
@@ -1,22 +1,8 @@
-const getToken = async () => {
-  console.log("getToken sis call ");
-  try {
-    let res2 = await localStorage.getItem("token");
-
-    if (res2 != null) {
-      console.log("TOKEN----------------------------------", JSON.parse(res2));
-      return JSON.parse(res2);
-    } else {
-      return null;
-    }
-  } catch (error) {
-    console.log(error);
-    return error;
-  }
-};
+import { store } from "../../src/modules/Store";
+export const token = store.getState().auth.token;
+export const server_url = "http://192.168.100.12:8000/api/";
 
 // export const server_url = "https://pak-group.herokuapp.com/";
-export const server_url = "http://192.168.100.12:8000/api/";
 // export const server_url = "http://192.168.0.105:8000/api/";
-export const token =
-  "eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJhdWQiOiIxIiwianRpIjoiMTVmYTQ2MDE5NmNkZTk2MmNmOTU4MjdjZmUwNjFhYzYyODRjMGExYzE5ZTFjN2RlMTk5Zjc1YjM1MWU1ZjNhYTZhMmU0ZTJkMzgxM2Y0YjUiLCJpYXQiOiIxNjEwMTczMDU5LjYwMTM5MyIsIm5iZiI6IjE2MTAxNzMwNTkuNjAxNDAxIiwiZXhwIjoiMTY0MTcwOTA1OS41ODQ1NTMiLCJzdWIiOiI3Iiwic2NvcGVzIjpbXX0.Az2WmafutZt0YIjFJp_QNTMqBfiomesTwn2s2y3jyW8nhPNJBAytqF_kWAZUrf4RNYVB2mDDIPpxHJyQqnPfuMi70MpovC68BEyNV8YDQn3lm93LMlxZ91txMQnZpLPYxLrcLD8rSsnA6TzLf8cMvYjOmXvLpiuNtkd-4ra_VBc4doFobh4LAVtE7eC739AzRkE9njihSSUNh3CA22vgiHNBPmR5_dXbMtzI_Ab5B11KEwzK5FjUsjkELSq_toc9KLTlQklKDQC0YTZ0NJ3bxrQjnAiceCnxuNyqMnFhZLYt6-VJixvnDraXbDmiWeyeUtq2LDgjn1vfm-fYwLiM8zOO8BHtJ7F43G-HG_hSlPKJq3a61NZoa_LM17tlYbrrLM8y35j3EAoNhUIdRH9yPA7UylEmpu9ceY7l8p7Th_yiG6Odu2eM-xaj23RGnrNnASNWzU7NKzPFTynvSeay_gS3hIvr6cui4uI6OOLu-nVjjfW4kzQqLUqUU69LEpgfaq_uKdiRHe1wO09rddrG-ybBdZ1Hya2ZT9f5KizgDDqleHCMccpn6ggqCRINM9bsNxqwxg5lCkk3n3aN519hDitgE27rmHxSz0mb6bMVdDyr39F9pilFytQHrXNUqADitbvSSY3axuf9xPiRYGeK53J4bj6mu4n0Ku4OW-SNif4";
+// export const token =
+//   "eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJhdWQiOiIxIiwianRpIjoiMTVmYTQ2MDE5NmNkZTk2MmNmOTU4MjdjZmUwNjFhYzYyODRjMGExYzE5ZTFjN2RlMTk5Zjc1YjM1MWU1ZjNhYTZhMmU0ZTJkMzgxM2Y0YjUiLCJpYXQiOiIxNjEwMTczMDU5LjYwMTM5MyIsIm5iZiI6IjE2MTAxNzMwNTkuNjAxNDAxIiwiZXhwIjoiMTY0MTcwOTA1OS41ODQ1NTMiLCJzdWIiOiI3Iiwic2NvcGVzIjpbXX0.Az2WmafutZt0YIjFJp_QNTMqBfiomesTwn2s2y3jyW8nhPNJBAytqF_kWAZUrf4RNYVB2mDDIPpxHJyQqnPfuMi70MpovC68BEyNV8YDQn3lm93LMlxZ91txMQnZpLPYxLrcLD8rSsnA6TzLf8cMvYjOmXvLpiuNtkd-4ra_VBc4doFobh4LAVtE7eC739AzRkE9njihSSUNh3CA22vgiHNBPmR5_dXbMtzI_Ab5B11KEwzK5FjUsjkELSq_toc9KLTlQklKDQC0YTZ0NJ3bxrQjnAiceCnxuNyqMnFhZLYt6-VJixvnDraXbDmiWeyeUtq2LDgjn1vfm-fYwLiM8zOO8BHtJ7F43G-HG_hSlPKJq3a61NZoa_LM17tlYbrrLM8y35j3EAoNhUIdRH9yPA7UylEmpu9ceY7l8p7Th_yiG6Odu2eM-xaj23RGnrNnASNWzU7NKzPFTynvSeay_gS3hIvr6cui4uI6OOLu-nVjjfW4kzQqLUqUU69LEpgfaq_uKdiRHe1wO09rddrG-ybBdZ1Hya2ZT9f5KizgDDqleHCMccpn6ggqCRINM9bsNxqwxg5lCkk3n3aN519hDitgE27rmHxSz0mb6bMVdDyr39F9pilFytQHrXNUqADitbvSSY3axuf9xPiRYGeK53J4bj6mu4n0Ku4OW-SNif4";
