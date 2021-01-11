bbc01f87819b1873a4e61319c0b2b8ee32606fba
syeda-Rabia <rabiagillani899@gmail.com>
2021-01-11 18:35:37 +0500

    get employee ang get inventory api's implemented

--- src/screens/Admin/ViewableTo/ViewableTo.js
+++ src/screens/Admin/ViewableTo/ViewableTo.js
@@ -15,32 +15,88 @@ export default function ViewableTo() {
   const [viewable, setViewable] = React.useState([]);
   const [name, setName] = React.useState([]);
   const [data, setData] = React.useState([]);
-
-  const Employees = [
+  let i = 0;
+  const [Employees, setEmployees] = React.useState([
     { label: "Sana", value: "Sana" },
     { label: "Atif", value: "Atif" },
     { label: "Ali", value: "Ali" },
     { label: "Imtesal", value: "Imtesal" },
     { label: "Rabia", value: "Rabia" },
     { label: "Qasim", value: "Qasim" },
-  ];
+  ]);
   const HandleName = (id) => {
     if (!select.includes(id)) setSelect((state) => [...state, id]);
     else setSelect((state) => state.filter((item) => item != id));
   };
-  console.log(select);
-  console.log(viewable);
-  console.log(name);
+  // console.log(select);
+  // console.log(viewable);
+  // console.log(name);
   const handleInventoryData = async () => {
     let res = await GET(ApiUrls.GET_ALL_VIEWABLE_INVENTORIES);
-    console.log(res);
+    console.log(res, "sana");
     if (res.success != false) {
-      // setData(res.data.ProjectCategory);
+      setData(res.data.projects.data);
     }
   };
+  const handleEmployeeName = async () => {
+    let res = await GET(ApiUrls.GET_ALL_EMPLOYEES);
+    console.log(res, "rabia");
+    if (res.success != false) {
+      // setViewable(res.data.users.data);
+      let arr = [];
+      res.data.users.map((item) => {
+        arr.push({ label: item.first_name, value: item.first_name });
+      });
+      setEmployees(arr);
+    }
+  };
+
   React.useEffect(() => {
     handleInventoryData();
+    handleEmployeeName();
   }, []);
+
+  const Table = ({ item, inventories, index }) => {
+    return (
+      <tr>
+        <td>
+          <input
+            type="checkBox"
+            onClick={(e) => {
+              HandleName(index);
+            }}
+          />
+        </td>
+        <td>{index}</td>
+        <td>{item.name}</td>
+        <td>{item.category.name}</td>
+        <td>{inventories.inventory_name}</td>
+        <td>{inventories.block_name}</td>
+        <td>{inventories.inventory_category}</td>
+        <td>{inventories.property_status}</td>
+        <td>
+          {viewable != null
+            ? viewable.map((task) => {
+                return `${task.value} `;
+              })
+            : null}
+        </td>
+      </tr>
+    );
+  };
+  const SelectData = async (event) => {
+    event.preventDefault();
+    let postData = {
+      // inventory_ids: id,
+      // user_ids: id,
+    };
+    let res = await POST(
+      ApiUrls.POST_ALL_SELECTED_EMPLOYEES_AND_INVENTORY,
+      postData
+    );
+    console.log(res);
+    let arr = data;
+  };
   return (
     <Container fluid>
       <div className="col-lg-12 shadow p-3 mb-3 bg-white rounded mt-4">
@@ -75,9 +131,7 @@ export default function ViewableTo() {
                       style={{ backgroundColor: "#2258BF" }}
                       // disabled={!select.every((v) => v === true)}
 
-                      onClick={(arr) => {
-                        console.log(arr);
-                      }}
+                      onClick={SelectData}
                     >
                       save
                     </button>
@@ -138,30 +192,18 @@ export default function ViewableTo() {
                   </tr>
                 </thead>
                 <tbody>
-                  <tr>
-                    <td scope="row">
-                      <input
-                        type="checkBox"
-                        onClick={(e) => {
-                          HandleName(0);
-                        }}
-                      />
-                    </td>
-                    <td>1</td>
-                    <td>Project Name</td>
-                    <td>LDA City</td>
-                    <td>DHA</td>
-                    <td>Block A</td>
-                    <td>Sale</td>
-                    <td>Sold</td>
-                    <td>
-                      {viewable != null
-                        ? viewable.map((task) => {
-                            return `${task.value} `;
-                          })
-                        : null}
-                    </td>
-                  </tr>
+                  {data.map((item, index) => {
+                    return item.inventories.map((inventories, id) => {
+                      i++;
+                      return (
+                        <Table
+                          item={item}
+                          inventories={inventories}
+                          index={i}
+                        />
+                      );
+                    });
+                  })}
                 </tbody>
               </table>
             </div>
